903143e5120c312ddef1456c7fafbcc8647a7e3d
David Y. Stephenson <david@davidystephenson.com>
2019-12-13 12:28:17 +0100

    Create router factory

--- index.js
+++ index.js
@@ -2,17 +2,45 @@ const express = require('express')
 const bodyParser = require(
   'body-parser'
 )
+const Sse = require('json-sse')
 
-const messageRouter = require('./message/router')
+const Message = require('./message/model')
+const messageRouterFactory = require('./message/router')
 
 const app = express()
 
 const port = 4000
 
+const stream = new Sse()
+const messageRouter = messageRouterFactory(stream)
+
 app.get(
   '/',
   (request, response) => {
+    stream.send('hi')
+
     response.send('hello')
+  }
+)
+
+app.get(
+  '/stream',
+  async (request, response, next) => {
+    try {
+      // Get array out of database
+      const messages = await Message.findAll()
+
+      // Convert array into string - "serialize" it
+      const string = JSON.stringify(messages)
+
+      // Prepare string to be sent to the client right after they *connect*
+      stream.updateInit(string)
+
+      // *Connect* the user to the stream
+      stream.init(request, response)
+    } catch (error) {
+      next(error) // handle any errors
+    }
   }
 )
 
--- message/router.js
+++ message/router.js
@@ -5,56 +5,63 @@ const Message = require('./model')
 
 const { Router } = express
 
-const router = new Router()
+function factory (stream) {
+  const router = new Router()
 
-router.get(
-  '/message',
-  async (request, response, next) => {
-    // Message
-    //   .findAll()
-    //   .then(messages => {
-    //     response.send(messages)
-    //   })
-    //   .catch(next)
+  router.get(
+    '/message',
+    async (request, response, next) => {
+      // Message
+      //   .findAll()
+      //   .then(messages => {
+      //     response.send(messages)
+      //   })
+      //   .catch(next)
 
+      try {
+        const messages = await Message
+          .findAll()
+
+        response.send(messages)
+      } catch (error) {
+        next(error)
+      }
+    }
+  )
+
+  // router.post(
+  //   '/message',
+  //   async (request, response, next) => {
+  //     try {
+  //       const message = await Message
+  //         .create(request.body)
+  //
+  //       response.send(message)
+  //     } catch (error) {
+  //       next(error)
+  //     }
+  //   }
+  // )
+
+  async function onPost (
+    request, response, next
+  ) {
     try {
-      const messages = await Message
-        .findAll()
+      const message = await Message
+        .create(request.body)
 
-      response.send(messages)
+      const string = JSON.stringify(message)
+      stream.send(string)
+
+      response.send(message)
     } catch (error) {
       next(error)
     }
   }
-)
 
-// router.post(
-//   '/message',
-//   async (request, response, next) => {
-//     try {
-//       const message = await Message
-//         .create(request.body)
-//
-//       response.send(message)
-//     } catch (error) {
-//       next(error)
-//     }
-//   }
-// )
+  router.post('/message', onPost)
 
-async function onPost (
-  request, response, next
-) {
-  try {
-    const message = await Message
-      .create(request.body)
-
-    response.send(message)
-  } catch (error) {
-    next(error)
-  }
+  return router
 }
 
-router.post('/message', onPost)
-
-module.exports = router
+module.exports = factory
